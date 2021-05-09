ace2fe20423fc6d35ba2500651911314b6bc7c3c
Debmalya Pramanik <DebmalyaPramanik.005@gmail.com>
2021-05-09 19:25:30 +0530

    Added CHANGELOG.md
    * corrected typos

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+All the notable changes for this project is documented here. This project lightly adhere to [Semantic Versioning](http://semver.org/).
+
+## 0.1-develop [development]
+A simple version of Principal Component Analysis (PCA) code is developed using the `numpy` library.
--- __init__.py
+++ __init__.py
@@ -8,10 +8,10 @@ __author_email__ = "Debmalya.18KT0018@ee.iitism.ac.in"
 __credits__      = None
 
 __status__       = "Development"
-__version__      = "0.1-deleop"
+__version__      = "0.1-develop"
 __docformat__    = "camelCasing"
 
-__copyright__    = "Copyright (c) 2021 Debmalya Pramanik"
+__copyright__    = "Copyright (c) 2021 Debmalya Pramanik, Arijit Baral"
 __affiliation__  = "Reliance Jio Platforms Ltd. | IIT Dhanbad"
 
 from .pca import PCA # Principal Component Analysis Algorithm
